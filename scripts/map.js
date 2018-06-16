ca87b0aaae6be7718b48f8dcec6117616f85ccde
Dimitar Sumerski <dsumerski@abv.bg>
2018-06-16 12:43:47 +0200

    Remove jQuery and switch to native JavaScript

--- scripts/map.js
+++ scripts/map.js
@@ -1,105 +1,100 @@
-$( document ).ready(function() {
-    var restaurantList = [
-    {
-        "latlng":[53.54861,9.9288813],
-        name:"Zur Traube"
-    },
 
-    {
-        "latlng":[53.5478089,9.9276869],
-        name:"Cuore Mio"
-    },
+var restaurantList = [
+{
+    "latlng":[53.54861,9.9288813],
+    name:"Zur Traube"
+},
+{
+    "latlng":[53.5478089,9.9276869],
+    name:"Cuore Mio"
+},
+{
+    "latlng":[53.5492824,9.9230596],
+    name:"Taverna Kreta"
+},
+{
+    "latlng":[53.550419,9.9247113],
+    name:"Brasserie La Provence"
+},
+{
+    "latlng":[53.5507854,9.9249039],
+    name:"Restaurant Kleine Brunnenstrasse"
+},
+{
+    "latlng":[53.55152,9.9287612],
+    name:"Cosmos Ristorante"
+},
+{
+    "latlng":[53.5520467,9.9309137],
+    name:"Schweinske in Ottensen"
+},
+{
+    "latlng":[53.5516211,9.9252909],
+    name:"Goldene Gans"
+},
+{
+    "latlng":[53.5491651,9.9201741],
+    name:"Fuh"
+},
+{
+    "latlng":[53.5540809,9.9224833],
+    name:"Restaurant Balutschistan Altona"
+}
+];
 
-    {
-        "latlng":[53.5492824,9.9230596],
-        name:"Taverna Kreta"
-    },
+var timeout, mapCanvas, zoom;
 
-    {
-        "latlng":[53.550419,9.9247113],
-        name:"Brasserie La Provence"
-    },
+function initialize() {
 
-    {
-        "latlng":[53.5507854,9.9249039],
-        name:"Restaurant Kleine Brunnenstrasse"
-    },
+    var mapDiv = document.getElementById("map");
+    mapCanvas = new google.maps.Map(mapDiv);
+    mapCanvas.setMapTypeId(google.maps.MapTypeId.SATELLITE);
 
-    {
-        "latlng":[53.55152,9.9287612],
-        name:"Cosmos Ristorante"
-    },
+    var bounds = new google.maps.LatLngBounds();
+    var restaurant, i, latlng;
 
-    {
-        "latlng":[53.5520467,9.9309137],
-        name:"Schweinske in Ottensen"
-    },
+    for (i in restaurantList) {
+        restaurant = restaurantList[i];
+        latlng = new google.maps.LatLng(restaurant.latlng[0], restaurant.latlng[1]);
+        bounds.extend(latlng);
+        var marker = createMarker(
+            mapCanvas, latlng, restaurant.name
+            );
 
-    {
-        "latlng":[53.5516211,9.9252909],
-        name:"Goldene Gans"
-    },
-
-    {
-        "latlng":[53.5491651,9.9201741],
-        name:"Fuh"
-    },
-
-    {
-        "latlng":[53.5540809,9.9224833],
-        name:"Restaurant Balutschistan Altona"
+        bindMarkerButton(marker, i);
     }
-    ];
-    
-    var timeout, mapCanvas, zoom;
-    
-    function initialize() {
-        var mapDiv = document.getElementById("map");
-        mapCanvas = new google.maps.Map(mapDiv);
-        mapCanvas.setMapTypeId(google.maps.MapTypeId.SATELLITE);
-  
-        var bounds = new google.maps.LatLngBounds();
-        var restaurant, i, latlng;
-        
-        for (i in restaurantList) {
-            restaurant = restaurantList[i];
-            latlng = new google.maps.LatLng(restaurant.latlng[0], restaurant.latlng[1]);
-            bounds.extend(latlng);
-            var marker = createMarker(
-                mapCanvas, latlng, restaurant.name
-                );
 
-            bindMarkerButton(marker, i);
-        }
+    mapCanvas.fitBounds(bounds);
+}
 
-        mapCanvas.fitBounds(bounds);
-    }
-    function createMarker(map, latlng) {
+function createMarker(map, latlng) {
 
-            var marker = new google.maps.Marker({
-            position : latlng,
-            map : map
-        });
+        var marker = new google.maps.Marker({
+        position : latlng,
+        map : map
+    });
 
-        google.maps.event.addListener(marker, "click", function(){ 
-            map.panTo(marker.getPosition());  
-            map.setZoom(18); 
-            
-            if(!timeout) {
-                $('.info-trigger').trigger('click');
-            }      
-            timeout = false;
-        });
-        return marker;
-    }
-    function bindMarkerButton(marker, i) {
-        var li = $($('.list__item')[i]);
+    google.maps.event.addListener(marker, "click", function(){ 
+        map.panTo(marker.getPosition());  
+        map.setZoom(18); 
 
-        google.maps.event.addDomListener(li[0], "click", function(){
-            timeout = setTimeout(function() {
-                google.maps.event.trigger(marker, "click");
-            }, 500);            
-        });
-    }
-    google.maps.event.addDomListener(window, "load", initialize);
-}); 
+        if(!timeout) {
+            document.querySelector('.info-trigger').click();
+        }      
+        timeout = false;
+    });
+    return marker;
+}
+
+function bindMarkerButton(marker, i) {
+    var li = document.querySelectorAll('.list__item');
+
+    google.maps.event.addDomListener(li[i], "click", function(){
+        timeout = setTimeout(function() {
+            google.maps.event.trigger(marker, "click");
+        }, 500);            
+    });
+}
+
+google.maps.event.addDomListener(window, "load", initialize);
+
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,27 @@
+    
+document.addEventListener("DOMContentLoaded", function() {
 
-( function( $ ) {
-    
-    $( document ).ready( function() {
-        $('.list__item').on('click', function() {
-            $('.list').addClass('list--off-canvas');
-            $('.navbar').addClass('navbar--on');
-        });
-        
-        $('.back-button').on('click', function() {
-            $('.list').removeClass('list--off-canvas');  
-            $('.navbar').removeClass('navbar--on');
-        });
-        
-        document.querySelector('.info-trigger').addEventListener('click', function() {
-            document.querySelector('.overlay').classList.add('overlay--active');
-        });
-    
-        document.querySelector('.overlay').addEventListener('click', function() {
-            this.classList.remove('overlay--active');
-        });
-    
-        document.querySelector('.info-window .content').addEventListener('click', function(event) {
-            event.stopPropagation();
+    document.querySelectorAll('.list__item').forEach(function(elem) {
+        elem.addEventListener('click', function() {
+            document.querySelector('.list').classList.add('list--off-canvas');
+            document.querySelector('.navbar').classList.add('navbar--on');
         });
     });
 
-} )( jQuery );
+    document.querySelector('.back-button').addEventListener('click', function() {
+        document.querySelector('.list').classList.remove('list--off-canvas');  
+        document.querySelector('.navbar').classList.remove('navbar--on');
+    });
 
-
+    document.querySelector('.info-trigger').addEventListener('click', function() {
+        document.querySelector('.overlay').classList.add('overlay--active');
+    });
+    
+    document.querySelector('.overlay').addEventListener('click', function() {
+        this.classList.remove('overlay--active');
+    })
+    
+    document.querySelector('.info-window .content').addEventListener('click', function(event) {
+        event.stopPropagation();
+    })
+});
